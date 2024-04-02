5529f8bce0fd288fe1537a41cac7e54acb77a4dd
Sangeetha5-C <165787124+Sangeetha5-C@users.noreply.github.com>
2024-04-02 16:35:29 +0530

    Add files via upload

---
+++ tour.js
@@ -0,0 +1,24 @@
+const counters = document.querySelectorAll('.num');
+const speed = 790;
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
+
+
+
+
